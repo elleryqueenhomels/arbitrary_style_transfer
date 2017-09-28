763e3232ce1ac967343bc13f04f98b96360d79f0
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-09-28 04:38:36 -0500

    Adaptive Instance Normalization (AdaIN layer)

---
+++ adaptive_instance_norm.py
@@ -0,0 +1,14 @@
+# Adaptive Instance Normalization
+
+import tensorflow as tf
+
+
+def AdaIN(content, style, epsilon=1e-5):
+    meanC, varC = tf.nn.moments(content, [1, 2], keep_dims=True)
+    meanS, varS = tf.nn.moments(style,   [1, 2], keep_dims=True)
+
+    sigmaC = tf.sqrt(tf.add(varC, epsilon))
+    sigmaS = tf.sqrt(tf.add(varS, epsilon))
+    
+    return (content - meanC) * sigmaS / sigmaC + meanS
+
