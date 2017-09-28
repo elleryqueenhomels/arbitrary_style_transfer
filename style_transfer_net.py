e258c19fe9fe9534c00ed945947dc5d46b444f60
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-09-28 04:39:28 -0500

    Style Transfer Network

---
+++ style_transfer_net.py
@@ -0,0 +1,49 @@
+# Style Transfer Network
+# Encoder -> AdaIN -> Decoder
+
+import tensorflow as tf
+
+from decoder import decode
+from encoder import Encoder
+from adaptive_instance_norm import AdaIN
+
+
+class StyleTransferNet(object):
+
+    def __init__(self, encoder_weights_path):
+        self.encoder_weights_path = encoder_weights_path
+
+    def transform(self, content, style):
+        # create encoder
+        encoder = Encoder(self.encoder_weights_path)
+        self.encoder = encoder
+
+        # switch RGB to BGR
+        content = tf.reverse(content, axis=[-1])
+        style   = tf.reverse(style,   axis=[-1])
+
+        # preprocess image
+        content = encoder.preprocess(content)
+        style   = encoder.preprocess(style)
+
+        # encode image
+        enc_c, enc_c_layers = encoder.encode(content)
+        enc_s, enc_s_layers = encoder.encode(style)
+
+        self.encoded_content_layers = enc_c_layers
+        self.encoded_style_layers   = enc_s_layers
+
+        # pass the encoded images to AdaIN
+        target_features = AdaIN(enc_c, enc_s)
+
+        # decode the target features back to image
+        generated_img = decode(target_features)
+
+        self.target_features = target_features
+        self.generated_img   = generated_img
+
+        # switch BGR back to RGB
+        output = tf.reverse(generated_img, axis=[-1])
+
+        return output
+
