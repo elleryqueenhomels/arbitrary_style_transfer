4ae90285be94637e111cfe67140e4491db4570e4
Wengao Ye <elleryqueenhomels@users.noreply.github.com>
2017-10-03 01:58:13 +0800

    Style Transfer Network

--- style_transfer_net.py
+++ style_transfer_net.py
@@ -3,32 +3,29 @@
 
 import tensorflow as tf
 
-from decoder import decode
 from encoder import Encoder
+from decoder import Decoder
 from adaptive_instance_norm import AdaIN
 
 
 class StyleTransferNet(object):
 
     def __init__(self, encoder_weights_path):
-        self.encoder_weights_path = encoder_weights_path
+        self.encoder = Encoder(encoder_weights_path)
+        self.decoder = Decoder()
 
     def transform(self, content, style):
-        # create encoder
-        encoder = Encoder(self.encoder_weights_path)
-        self.encoder = encoder
-
         # switch RGB to BGR
         content = tf.reverse(content, axis=[-1])
         style   = tf.reverse(style,   axis=[-1])
 
         # preprocess image
-        content = encoder.preprocess(content)
-        style   = encoder.preprocess(style)
+        content = self.encoder.preprocess(content)
+        style   = self.encoder.preprocess(style)
 
         # encode image
-        enc_c, enc_c_layers = encoder.encode(content)
-        enc_s, enc_s_layers = encoder.encode(style)
+        enc_c, enc_c_layers = self.encoder.encode(content)
+        enc_s, enc_s_layers = self.encoder.encode(style)
 
         self.encoded_content_layers = enc_c_layers
         self.encoded_style_layers   = enc_s_layers
@@ -37,14 +34,17 @@ class StyleTransferNet(object):
         target_features = AdaIN(enc_c, enc_s)
         self.target_features = target_features
 
-        # decode the target features back to image
-        generated_img = decode(target_features)
+        # decode target features back to image
+        generated_img = self.decoder.decode(target_features)
 
         # deprocess image
-        generated_img = encoder.deprocess(generated_img)
+        generated_img = self.encoder.deprocess(generated_img)
 
         # switch BGR back to RGB
-        output = tf.reverse(generated_img, axis=[-1])
+        generated_img = tf.reverse(generated_img, axis=[-1])
 
-        return output
+        # clip to 0..255
+        generated_img = tf.clip_by_value(generated_img, 0.0, 255.0)
+
+        return generated_img
 
