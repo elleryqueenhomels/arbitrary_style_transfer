18f07fbda8e599aa1ebb70e278001e1c24459483
Wengao Ye <elleryqueenhomels@hotmail.com>
2017-11-29 23:12:40 +0800

    Update and rename download.sh to download_vgg19.sh

--- download.sh
+++
@@ -1,3 +0,0 @@
-wget -c https://s3-us-west-2.amazonaws.com/wengaoye/vgg19_normalised.npz
-wget -c https://s3-us-west-2.amazonaws.com/wengaoye/arbitrary_style_model_style-weight-2e0.zip
-unzip arbitrary_style_model_style-weight-2e0.zip
---
+++ download_vgg19.sh
@@ -0,0 +1,1 @@
+wget -c https://s3-us-west-2.amazonaws.com/wengaoye/vgg19_normalised.npz
