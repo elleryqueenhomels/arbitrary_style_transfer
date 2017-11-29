fa593a8c1de31f71648fc6e7d9702dff02e95314
Wengao Ye <elleryqueenhomels@hotmail.com>
2017-11-29 21:56:09 +0800

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,3 @@
+wget -c https://s3-us-west-2.amazonaws.com/wengaoye/vgg19_normalised.npz
+wget -c https://s3-us-west-2.amazonaws.com/wengaoye/arbitrary_style_model_style-weight-2e0.zip
+unzip arbitrary_style_model_style-weight-2e0.zip
