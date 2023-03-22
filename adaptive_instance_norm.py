084e548dbbc9393ce76a1b2bd534bd3e9ab39438
Wengao Ye <elleryqueenhomels@hotmail.com>
2023-03-22 23:21:03 +0000

    Refine AdaIn function

--- adaptive_instance_norm.py
+++ adaptive_instance_norm.py
@@ -2,13 +2,11 @@
 
 import tensorflow as tf
 
-
 def AdaIN(content, style, epsilon=1e-5):
-    meanC, varC = tf.nn.moments(content, [1, 2], keep_dims=True)
-    meanS, varS = tf.nn.moments(style,   [1, 2], keep_dims=True)
+    mean_c, var_c = tf.nn.moments(content, [1, 2], keep_dims=True)
+    mean_s, var_s = tf.nn.moments(style,   [1, 2], keep_dims=True)
 
-    sigmaC = tf.sqrt(tf.add(varC, epsilon))
-    sigmaS = tf.sqrt(tf.add(varS, epsilon))
+    sigma_c = tf.sqrt(tf.add(var_c, epsilon))
+    sigma_s = tf.sqrt(tf.add(var_s, epsilon))
     
-    return (content - meanC) * sigmaS / sigmaC + meanS
-
+    return (content - mean_c) * sigma_s / sigma_c + mean_s
